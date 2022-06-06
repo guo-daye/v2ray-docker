0e97bc7864b11336048a8c4a17c7a1812eab4c2c
guo-daye <62917902+guo-daye@users.noreply.github.com>
2022-06-06 18:05:55 +0800

    Update xf.sh

--- xf.sh
+++ xf.sh
@@ -60,7 +60,7 @@ cat <<EOF >/etc/v2ray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "11111111-1111-1111-1111-111111111111",
+                        "id": "33eedfae-e580-11ec-8fea-0242ac120002",
                         "alterId": 0
                     }
                 ],
